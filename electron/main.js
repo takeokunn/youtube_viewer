e5192aaf3d6e40483a54757abd52f7dca52ba449
takeokunn <bararararatty@gmail.com>
2018-09-23 12:18:56 +0900

    Update electron config

--- electron/main.js
+++ electron/main.js
@@ -7,16 +7,20 @@ let mainWindow = null;
 const INITIAL_WINDOW = {
     WIDTH: 1200,
     HEIGHT: 800
-}
+};
+
+process.on('uncaughtException', err => console.log('uncaughtException occured'));
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('ready', () => {
+const create_browser_window = () => {
+    if (mainWindow !== null) return;
     mainWindow = new BrowserWindow({ width: INITIAL_WINDOW.WIDTH, height: INITIAL_WINDOW.HEIGHT });
     mainWindow.loadURL('file://' + __dirname + '/../public/index.html');
     mainWindow.on('closed', () => mainWindow = null);
-});
+};
+
+app.on('ready', create_browser_window);
+app.on('activate', create_browser_window);
